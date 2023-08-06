33c289e2b4ad67eaf603162b657c1e9fde04f935
Lazhar.BK <lbenkhlifa@bulldozerinc.com>
2023-08-06 01:57:21 +0100

    First commit

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities;
+
+public class AppUser
+{
+    [Key] //annotation is not needed here, as Id is conventionally named
+    public int Id { get; set; }
+    public string UserName { get; set; }
+}
